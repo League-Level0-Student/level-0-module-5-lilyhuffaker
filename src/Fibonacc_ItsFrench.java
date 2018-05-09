0b04f2ba6d27d895ce98a6696a9009a3caff7dd6
Huffaker.Lily <Huffaker.Lily@SEMS-259-32.SEMS259.LAB>
2018-05-09 16:21:31 -0700

    UMMMMMMMMMM...U did stuff

---
+++ src/Fibonacc_ItsFrench.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Fibonacc_ItsFrench {
+public static void main(String[] args) {
+	int num1 = 1;
+	int num2 = 1;
+	String nerm = JOptionPane.showInputDialog("Dr. Nugget: How many Fobonacci number sequesnces would you like?");
+	int fib = Integer.parseInt(nerm);
+	System.out.println(1);
+	System.out.println(1);
+	for(int i=0;i<fib;i++){
+		int sum=num1+num2;
+		System.out.println(sum);
+		num1=num2;
+		num2=sum;
+	}
+	
+	
+	
+}
+}
---
+++ src/PrimeOrNOOOOOT.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class PrimeOrNOOOOOT {
+public static void main(String[] args) {
+	
+String pos=JOptionPane.showInputDialog("Dr. Nugget: Hello, give me a positive number to continue.");	
+	int nerm=Integer.parseInt(pos);
+
+	if(nerm==1) {
+		JOptionPane.showMessageDialog(null, "Dr. Nugget: Your number, "+pos+ " is not prime.");
+		return;
+	}
+	if(nerm==2) {
+		JOptionPane.showMessageDialog(null, "Dr. Nugget: Your number, "+pos+" is prime.");
+		return;
+	}
+	
+	for(int i=3;i<nerm;i+=2) {
+		if(nerm%i==0) {
+			JOptionPane.showMessageDialog(null, "Dr. Nugget: Your number, "+pos+ " is not prime.");
+			return;
+		}
+	}
+	JOptionPane.showMessageDialog(null, "Dr. Nugget: Your number, "+pos+" is prime.");
+	return;
+}
+}
+
+//lily: 12-9-12-25
